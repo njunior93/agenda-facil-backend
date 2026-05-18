14b4bd90fe2903a7c39acb36c57e47b7eeb0cd06
Natanael Junior <naeljunior07@gmail.com>
2026-05-18 19:45:57 -0300

    teste

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsuarioModule } from 'src/app/modulos/usuario/usuario.module';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { Usuario } from 'src/app/modulos/usuario/entities/usuario.entity';
